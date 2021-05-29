20456c7a2688732293aee495d6d5b77426f622eb
Tchelet777 <85037432+Tchelet777@users.noreply.github.com>
2021-05-29 23:49:42 +0300

    Update clone.py

--- clone.py
+++ clone.py
@@ -5,12 +5,13 @@ parser = argparse.ArgumentParser(description="clone repository")
 parser.add_argument('-v', '--verbose',  action='store_true', required=False, default='', help='Run verbosely. Does not affect the reporting of progress status to the standard error stream.')
 args = parser.parse_args()
 
+
 def main(verbose):
-    postfix = ''
+    suffix = ''
     if verbose:
-        postfix = '--verbose'
-    print(postfix)
-    subprocess.call('git clone https://github.com/Tchelet777/python-example.git ' + postfix, shell=True)
+        suffix = '--verbose'
+    subprocess.call('git clone https://github.com/Tchelet777/python-example.git ' + suffix, shell=True)
+
 
 if __name__ == '__main__':
     main(verbose=args.verbose)
